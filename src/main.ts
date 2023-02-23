0be0f3bb1ea35cbc4fdf48906f71a6e1b1767d33
Temka Artemka <yandex949@gmail.com>
2023-02-23 19:04:50 +0300

    docs: swagger custom js,css

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,20 @@ import * as cookieParser from 'cookie-parser'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
+const opts = {
+  customSiteTitle: 'Zavod REST API DOCS',
+  customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+  ],
+  customCssUrl: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+  ],
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
@@ -21,9 +35,7 @@ async function bootstrap() {
     .addTag('Post')
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api-docs', app, document, {
-    customSiteTitle: 'Zavod REST API DOCS',
-  })
+  SwaggerModule.setup('api-docs', app, document, opts)
 
   await app.listen(2000)
 }
