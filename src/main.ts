3d7091b9030bc4270aff0558ede71e3598ab578b
Temka Artemka <yandex949@gmail.com>
2023-03-08 12:14:24 +0300

    fix: cors confgured

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api')
   app.enableCors({
-    origin: true,
+    origin: false,
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   })
